5c426ee88f66bfde74f7f7bd53bf94c579638c2c
Michael <simmichaeld@gmail.com>
2025-11-09 20:17:44 -0500

    added which key delay

--- lua/user/plugins/which-key.lua
+++ lua/user/plugins/which-key.lua
@@ -19,7 +19,7 @@ return {
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
       -- this setting is independent of vim.o.timeoutlen
-      delay = 0,
+      delay = 250,
       icons = {
         -- set icon mappings to true if you have a Nerd Font
         mappings = vim.g.have_nerd_font,
